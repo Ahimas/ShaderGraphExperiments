1df04f5bd9f7881cb99c1a2627ee4f2a4996341e
Vladimir Grygoryev <vladgrygoryev@gmail.com>
2023-12-28 19:01:07 +0200

    Added models and textures

---
+++ Assets/Scripts/DebugVertices.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class DebugVertices : MonoBehaviour {
+
+	private Vector3[] _vertices;
+
+	private void OnDrawGizmos()
+	{
+		
+		_vertices ??= GetComponent<MeshFilter>().sharedMesh.vertices;
+
+		foreach (Vector3 v in _vertices)
+            UnityEditor.Handles.Label(transform.position + v, "v: " + v.ToString());
+	}
+}
